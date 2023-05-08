3f3fa551b8ad3db6b954bcc05f645fcc259d957e
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2023-05-09 02:22:43 +0300

    commit message

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('./src/style.css')  //input
+    eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addFilter("luxonDate", date => {
+      return DateTime.fromJSDate(date).toLocaleString(DateTime.DATE_MED);
+    });
+    return {
+      dir: {
+        input: "src",
+        output: "public"
+      }
+    }
+  };
