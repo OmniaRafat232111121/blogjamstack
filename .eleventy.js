6e88f8bd4d0096c64a436987599b2015e40de41d
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2023-05-09 11:44:44 +0300

    cmsystem

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,8 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css')  //input
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin');
+
     eleventyConfig.addFilter("luxonDate", date => {
       return DateTime.fromJSDate(date).toLocaleString(DateTime.DATE_MED);
     });
